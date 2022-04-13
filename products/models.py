ab1e1ccf9c4cebaf7bde9594f4f147f92af009ee
leeceo97 <leeceo97@naver.com>
2022-04-13 15:05:27 +0900

    (feat)modeling

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Product(models.Model):
+    publisher = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='posted_product')
+    title = models.CharField(max_length=31)
+    description = models.TextField()
+    target_amount = models.PositiveIntegerField()
+    total_amount = models.PositiveIntegerField()
+    sponsors = models.ManyToManyField(
+        'accounts.User',
+        through='products.Sponsor',
+        through_fields=('product', 'sponsor'),
+    )
+    deadline = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Sponsor(models.Model):
+    sponsor = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    funding_amount = models.PositiveIntegerField()
